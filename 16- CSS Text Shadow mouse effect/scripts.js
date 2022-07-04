9a93c564712bfb9441c45b7096bbc6ffa110622e
Charli <zatoichihanzo@gmail.com>
2022-07-04 14:58:31 +0200

    workn

--- 16- CSS Text Shadow mouse effect/scripts.js
+++ 16- CSS Text Shadow mouse effect/scripts.js
@@ -1,9 +1,33 @@
 const hero = document.querySelector(".hero");
-const text = document.querySelector("h1");
+const text = hero.querySelector("h1");
 
-function shadow (e) => {
+function shadow(e) {
+  //get width and height of where we hover over
 
+  /*  const width = hero.offsetWidth;
+  const height = hero.offsetHeight; */
+  // Above line, but in destructuring :
+  const { offsetWidth: width, offsetHeight: height } = hero;
+
+  // where the cursors is :
+  /* let x = e.offsetX;
+  let y = e.offsetY; */
+  let { offsetX : x, offsetY : y } = e;
+
+  // get the position of the cursor :
+ console.log(x, y);
+  console.log(e);
+
+// 
+if(this !== e.target) {
+  x = x + e.target.offsetLeft;
+  y = y + e.target.offsetTop;
 }
 
+
+
+
+
+}
 
 hero.addEventListener('mousemove', shadow);
