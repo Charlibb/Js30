b78fc21a26a7e76d8d42922b4ff6609cde3e156b
Carlos B Borruel <zatoichihanzo@gmail.com>
2024-03-06 13:00:20 +0100

    21- Added extra Geolocation button for local host

--- 21 - Geolocation/script.js
+++ 21 - Geolocation/script.js
@@ -6,3 +6,37 @@ console.log('hello there ');
 navigator.geolocation.watchPosition((data) => {
   console.log(data);
 });
+
+// find me button func https://developer.mozilla.org/en-US/docs/Web/API/Geolocation_API/Using_the_Geolocation_API
+
+function geoFindMe() {
+  console.log('geo finding me!');
+  const status = document.querySelector('#status');
+  const mapLink = document.querySelector('#map-link');
+
+  mapLink.href = '';
+  mapLink.textContent = '';
+
+  function success(position) {
+    const latitude = position.coords.latitude;
+    const longitude = position.coords.longitude;
+
+    status.textContent = '';
+    mapLink.href = `https://www.openstreetmap.org/#map=18/${latitude}/${longitude}`;
+    mapLink.textContent = `Latitude: ${latitude} °, Longitude: ${longitude} °`;
+  }
+
+  function error(error) {
+    console.log(error.message);
+    status.textContent = 'Unable to retrieve your location';
+  }
+
+  if (!navigator.geolocation) {
+    status.textContent = 'Geolocation is not supported by your browser';
+  } else {
+    status.textContent = 'Locating…';
+    navigator.geolocation.getCurrentPosition(success, error);
+  }
+}
+
+document.querySelector('#find-me').addEventListener('click', geoFindMe);
