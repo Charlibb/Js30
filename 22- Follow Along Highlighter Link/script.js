bf03ed68f0eddc3484f6c86d9ed16758f1f5e5eb
Carlos B Borruel <zatoichihanzo@gmail.com>
2024-03-07 15:11:46 +0100

    22 Js basics

---
+++ 22- Follow Along Highlighter Link/script.js
@@ -0,0 +1,18 @@
+const triggers = document.querySelectorAll('a');
+const highlight = document.createElement('span');
+
+highlight.classList.add('highlight');
+document.body.append(highlight);
+
+function highlightLink() {
+  console.log();
+  console.log(this, ' was highlighted!');
+  const linkCoords = this.getBoundingClientRect();
+  console.log('linkCoords are: ', linkCoords);
+
+  highlight.style.width = `${linkCoords.width}px`;
+  highlight.style.height = `${linkCoords.height}px`;
+  highlight.style.transform = `translate(${linkCoords.left}px, ${linkCoords.top}px)`;
+}
+
+triggers.forEach((a) => a.addEventListener('mouseenter', highlightLink));
