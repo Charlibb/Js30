ab32367b755904c5ef066f1007164bbe1a496465
Carlos B Borruel <zatoichihanzo@gmail.com>
2024-05-22 17:30:48 +0200

    26- added coords bounding

--- 26 - Stripe Follow Along Nav/script.js
+++ 26 - Stripe Follow Along Nav/script.js
@@ -1,22 +1,32 @@
 const triggers = document.querySelectorAll('.cool > li');
-const dropDown = document.querySelector('.dropdownBackground');
+const background = document.querySelector('.dropdownBackground');
 const nav = document.querySelector('.top');
 
 function handleEnter(e) {
   console.log('entered');
   this.classList.add('trigger-enter');
-  setTimeout(() => {
-    this.classList.add('trigger-enter-active');
-  }, 200);
+  setTimeout(() => this.classList.add('trigger-enter-active'), 500);
+  background.classList.add('open');
+
+  const dropdown = this.querySelector('.dropdown');
+  const dropdownCoords = dropdown.getBoundingClientRect();
+  const navCoords = nav.getBoundingClientRect();
+
+  const coords = {
+    height: dropdownCoords.height,
+    width: dropdownCoords.width,
+  };
+
+  background.style.setProperty('width', `${coords.width}px`);
+  background.style.setProperty('height', `${coords.height}px`);
 }
 
 function handleLeave(e) {
   console.log('leaves');
 
   this.classList.remove('trigger-enter');
-  setTimeout(() => {
-    this.classList.remove('trigger-enter-active');
-  }, 500);
+  setTimeout(() => this.classList.remove('trigger-enter-active'), 500);
+  background.classList.remove('open');
 }
 
 triggers.forEach((trigger) =>
