c7cdf8c47bab6daf1556dd474426b7c70280e96b
Carlos B Borruel <zatoichihanzo@gmail.com>
2024-06-18 21:01:38 +0200

    30 added MolePeep & gameStart

--- 30 - Whack A Mole/script.js
+++ 30 - Whack A Mole/script.js
@@ -2,6 +2,7 @@ const holes = document.querySelectorAll('.hole');
 const scoreBoard = document.querySelector('.score');
 const moles = document.querySelectorAll('.mole');
 let lastHole;
+let timeUp = false;
 
 function randomTime(min, max) {
   return Math.round(Math.random() * (max - min) + min);
@@ -19,4 +20,23 @@ function randomHole(holes) {
   return hole;
 }
 
+function showMole() {
+  const time = randomTime(200, 1000);
+  const hole = randomHole(holes);
+  hole.classList.add('up');
+  setTimeout(() => {
+    hole.classList.remove('up');
+    if (!timeUp) {
+      showMole();
+    }
+  }, time);
+}
 
+function startGame() {
+  scoreBoard.textContent = 0;
+  timeUp = false;
+  showMole();
+  setTimeout(() => {
+    timeUp = true;
+  }, 10000);
+}
